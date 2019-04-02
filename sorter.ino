efecaaa3e20fcb7873a1b4387248fd4eb00e11dd
Zuzanna Kasper <35754359+zuzkasper@users.noreply.github.com>
2019-04-02 00:08:47 -0400

    Update sorter.ino

--- sorter.ino
+++ sorter.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#include <LiquidCrystal.h>
 
 //Color numbers
 #define RED 1
@@ -28,6 +29,10 @@ int blueHigh=-1;
 #define sol3 11
 #define sol4 12
 
+// LCD connections
+const int rs = A0, en = A1, d4 = A2, d5 = A3, d6 = A4, d7 = A5;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 // define servos
 Servo servo1; //feeder
 Servo servo2; //conveyer belt
@@ -52,13 +57,49 @@ int blueFrequency = 0;
 // initialize position variable for servos
 int pos=0;
 
-int calibrated=0;
+void moveFeeder(){
+  for (pos = 0; pos <= 140; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    servo1.write(pos);              // tell servo to go to position in variable 'pos'                   
+    delay(5);// waits 15ms for the servo to reach the position
+  }
+  delay(800);
+  for (pos = 140; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    servo1.write(pos);              // tell servo to go to position in variable 'pos'                 
+    delay(5);// waits 15ms for the servo to reach the position
+  }
+  delay(250);
+}
+
+void moveConveyer(){
+  int i=0;
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    servo2.write(pos);              // tell servo to go to position in variable 'pos'                   
+    delay(5);// waits 15ms for the servo to reach the position
+  }
+  delay(800);
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    servo2.write(pos);              // tell servo to go to position in variable 'pos'                 
+    delay(5);// waits 15ms for the servo to reach the position
+  }
+
+  //move color info 1 station to match conveyer
+  for (i=4; i>0; i--){
+    stations[i]=stations[i-1];
+  }
+}
 
 /*
  * Initial set up of sorter
  */
 
 void setup() {
+  // set up LCD columns and rows
+  lcd.begin(16,2);
+  //Below LCD HELLO WORLD Test
+  lcd.print("HAO LIN <3");
+  
   //Servo setup
   servo1.attach(servopin1);
   servo2.attach(servopin2);
@@ -81,7 +122,7 @@ void setup() {
   digitalWrite(sol3,HIGH);
   digitalWrite(sol4,HIGH);
   
-  // Setting the sensorOut as an input
+  // SettingsorOut as an input
   pinMode(sensorOut, INPUT);
   
   // Setting frequency scaling to 20%
@@ -90,7 +131,9 @@ void setup() {
   
    // Begins serial communication 
   Serial.begin(9600);
+  Serial.print("Hello World!");//DELETE
 
+  //calibrate colors
   calibration();
 }
 
@@ -101,25 +144,20 @@ void setup() {
 
 void loop() {
 
- /* if(calibrated==0){
-    calibrated=1;
-    calibration();
-  }*/
+  /*
+   * Calibrate the color frequencies
+   */
+  //if(calibrated==0){
+  //  calibration();
+  //  calibrated=1;
+  //}
   int i=0;
   /*
    * 1. move feeder full motion
    */
-  for (pos = 0; pos <= 160; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    servo1.write(pos);              // tell servo to go to position in variable 'pos'                   
-    delay(5);// waits 15ms for the servo to reach the position
-  }
-  delay(800);
-  for (pos = 160; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    servo1.write(pos);              // tell servo to go to position in variable 'pos'                 
-    delay(5);// waits 15ms for the servo to reach the position
-  }
-  delay(250);
+   //delay(100);
+   moveFeeder();
+   //delay(100);
   
   /* 
    * 2. Get color of item
@@ -134,35 +172,21 @@ void loop() {
    */
   //i.e. if color at a station matches the color that gets blasted there
   for (i=1; i<5; ++i){
-    Serial.print("\n");
-    Serial.print(stations[i]);
-    Serial.print("\n");
     if (stations[i]==i){   //if item is in right spot, push it off
       digitalWrite(stationPins[i],LOW);   //turn on solenoid
       delay(250);     //keep solonoid on for 250ms
       digitalWrite(stationPins[i], HIGH); //turn off solenoid
-      stations[i]=0;
-    }
+     stations[i]=0;
+     }
   }
   
   /*
    * 4.move conveyer 1 chain length and move color info to match
    */
-  for (pos = 0; pos <= 160; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    servo2.write(pos);              // tell servo to go to position in variable 'pos'                   
-    delay(5);// waits 15ms for the servo to reach the position
-  }
-  delay(800);
-  for (pos = 160; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    servo2.write(pos);              // tell servo to go to position in variable 'pos'                 
-    delay(5);// waits 15ms for the servo to reach the position
-  }
-
-  //move color info 1 station to match conveyer
-  for (i=4; i>0; i--){
-    stations[i]=stations[i-1];
-  }
+   //delay(100);
+   moveConveyer();
+   //delay(100);
+  
 }
 
 /*
@@ -171,208 +195,106 @@ void loop() {
  * void for now
  */
 
+
  int getColor(){
-  Serial.print("Getting Color ");
-  // Setting RED (R) filtered photodiodes to be read
-  digitalWrite(S2,LOW);
-  digitalWrite(S3,LOW);
-  
-  // Reading the output frequency
-  redFrequency = pulseIn(sensorOut, LOW);
-  
-   // Printing the RED (R) value
-  Serial.print("R = ");
-  Serial.print(redFrequency);
-  delay(100);
-  
-  // Setting GREEN (G) filtered photodiodes to be read
-  digitalWrite(S2,HIGH);
-  digitalWrite(S3,HIGH);
-  
-  // Reading the output frequency
-  greenFrequency = pulseIn(sensorOut, LOW);
-  
-  // Printing the GREEN (G) value  
-  Serial.print(" G = ");
-  Serial.print(greenFrequency);
-  delay(100);
- 
-  // Setting BLUE (B) filtered photodiodes to be read
-  digitalWrite(S2,LOW);
-  digitalWrite(S3,HIGH);
-  
-  // Reading the output frequency
-  blueFrequency = pulseIn(sensorOut, LOW);
-  
-  // Printing the BLUE (B) value 
-  Serial.print(" B = ");
-  Serial.println(blueFrequency);
-  delay(100);
+  getFrequencies();
   int i=1;
   while(i<5){
     //Serial.print(i);
     if(redFrequency>colors[i-1][0]&&redFrequency<colors[i-1][1] && greenFrequency>colors[i-1][2]&&greenFrequency<colors[i-1][3] && blueFrequency>colors[i-1][4]&&blueFrequency<colors[i-1][5]){
-      Serial.print("\nColor shot: ");
+      Serial.print("Color: ");
       Serial.print(i);
-      Serial.print("\n");
       return i;
     }
     i++;
   }
-  return 0;
  }
 
  //calibrates RED, GREEN, BLUE, YELLOW Frequencies
 void calibration(){
-  int count=0;
-  delay(10000);
-  Serial.print("Red: ");
-  while(count<50){
-    getFrequencies();
-    if (redLow==-1 || redLow>redFrequency){
-      redLow=redFrequency-5;
-    }
-    if (redHigh==-1 || redHigh<redFrequency){
-      redHigh=redFrequency+5;
-    }
-    if (greenLow==-1 || greenLow>greenFrequency){
-      greenLow=greenFrequency-5;
-    }
-    if (greenHigh==-1 || greenHigh<greenFrequency){
-      greenHigh=greenFrequency+5;
-    }
-    if (blueLow==-1 || blueLow>blueFrequency){
-      blueLow=blueFrequency-5;
-    }
-    if (blueHigh==-1 || blueHigh<blueFrequency){
-      blueHigh=blueFrequency+5;
-    }
-    count++;
-  }
-  colors[0][0]=redLow;
-  colors[0][1]=redHigh;
-  colors[0][2]=greenLow;
-  colors[0][3]=greenHigh;
-  colors[0][4]=blueLow;
-  colors[0][5]=blueHigh;
-  delay(10000);
-  redLow=-1;
-  redHigh=-1;
-  greenLow=-1;
-  greenHigh=-1;
-  blueLow=-1;
-  blueHigh=-1;
-  Serial.print("Green: ");
-  count=0;
-  while(count<50){
-    getFrequencies();
-    if (redLow==-1 || redLow>redFrequency){
-      redLow=redFrequency-5;
-    }
-    if (redHigh==-1 || redHigh<redFrequency){
-      redHigh=redFrequency+5;
-    }
-    if (greenLow==-1 || greenLow>greenFrequency){
-      greenLow=greenFrequency-5;
-    }
-    if (greenHigh==-1 || greenHigh<greenFrequency){
-      greenHigh=greenFrequency+5;
-    }
-    if (blueLow==-1 || blueLow>blueFrequency){
-      blueLow=blueFrequency-5;
-    }
-    if (blueHigh==-1 || blueHigh<blueFrequency){
-      blueHigh=blueFrequency+5;
-    }
-    count++;
-  }
-  colors[1][0]=redLow;
-  colors[1][1]=redHigh;
-  colors[1][2]=greenLow;
-  colors[1][3]=greenHigh;
-  colors[1][4]=blueLow;
-  colors[1][5]=blueHigh;
-  delay(10000);
-  redLow=-1;
-  redHigh=-1;
-  greenLow=-1;
-  greenHigh=-1;
-  blueLow=-1;
-  blueHigh=-1;
-  count=0;
-  Serial.print("Blue: ");
-  while(count<50){
-    getFrequencies();
-    if (redLow==-1 || redLow>redFrequency){
-      redLow=redFrequency-5;
-    }
-    if (redHigh==-1 || redHigh<redFrequency){
-      redHigh=redFrequency+5;
-    }
-    if (greenLow==-1 || greenLow>greenFrequency){
-      greenLow=greenFrequency-5;
-    }
-    if (greenHigh==-1 || greenHigh<greenFrequency){
-      greenHigh=greenFrequency+5;
-    }
-    if (blueLow==-1 || blueLow>blueFrequency){
-      blueLow=blueFrequency-5;
-    }
-    if (blueHigh==-1 || blueHigh<blueFrequency){
-      blueHigh=blueFrequency+5;
-    }
-    count++;
-  }
-  colors[2][0]=redLow;
-  colors[2][1]=redHigh;
-  colors[2][2]=greenLow;
-  colors[2][3]=greenHigh;
-  colors[2][4]=blueLow;
-  colors[2][5]=blueHigh;
-  delay(10000);
-  redLow=-1;
-  redHigh=-1;
-  greenLow=-1;
-  greenHigh=-1;
-  blueLow=-1;
-  blueHigh=-1;
-  count=0;
-  Serial.print("Yellow: ");
-  while(count<50){
-    getFrequencies();
-    if (redLow==-1 || redLow>redFrequency){
-      redLow=redFrequency-5;
-    }
-    if (redHigh==-1 || redHigh<redFrequency){
-      redHigh=redFrequency+5;
-    }
-    if (greenLow==-1 || greenLow>greenFrequency){
-      greenLow=greenFrequency-5;
-    }
-    if (greenHigh==-1 || greenHigh<greenFrequency){
-      greenHigh=greenFrequency+5;
-    }
-    if (blueLow==-1 || blueLow>blueFrequency){
-      blueLow=blueFrequency-5;
-    }
-    if (blueHigh==-1 || blueHigh<blueFrequency){
-      blueHigh=blueFrequency+5;
-    }
-    count++;
-  }
-  colors[3][0]=redLow;
-  colors[3][1]=redHigh;
-  colors[3][2]=greenLow;
-  colors[3][3]=greenHigh;
-  colors[3][4]=blueLow;
-  colors[3][5]=blueHigh;
-  redLow=-1;
-  redHigh=-1;
-  greenLow=-1;
-  greenHigh=-1;
-  blueLow=-1;
-  blueHigh=-1;
+  Serial.print("Calibration\n");
   int i=0;
+  servo1.write(0);
+  servo2.write(0);
+  //delay(100);
+  //moveFeeder();
+  Serial.print("MoveConveyer\n");
+  moveConveyer();
+  //delay(100);
+  while(i<4){
+    //delay(100);
+    Serial.print("MoveFeeder\n");
+    moveFeeder();
+    //delay(100);
+    Serial.print("MoveConveyer\n");
+    moveConveyer();
+    //delay(100);
+    int count=0;
+    Serial.print("Color: ");
+    Serial.print(i);
+    Serial.print("\n");
+    while(count<25){
+      getFrequencies();
+      if (redLow==-1 || redLow>redFrequency){
+        redLow=redFrequency-5;
+      }
+      if (redHigh==-1 || redHigh<redFrequency){
+        redHigh=redFrequency+5;
+      }
+      if (greenLow==-1 || greenLow>greenFrequency){
+        greenLow=greenFrequency-5;
+      }
+      if (greenHigh==-1 || greenHigh<greenFrequency){
+        greenHigh=greenFrequency+5;
+      }
+      if (blueLow==-1 || blueLow>blueFrequency){
+        blueLow=blueFrequency-5;
+      }
+      if (blueHigh==-1 || blueHigh<blueFrequency){
+        blueHigh=blueFrequency+5;
+      }
+      count++;
+    }
+    if(i==1){
+      digitalWrite(sol1,LOW);   //turn on solenoid
+      delay(250);     //keep solonoid on for 250ms
+      digitalWrite(sol1, HIGH); //turn off solenoid
+    }
+    if(i==3){
+      digitalWrite(sol2,LOW);   //turn on solenoid
+      delay(250);     //keep solonoid on for 250ms
+      digitalWrite(sol2, HIGH); //turn off solenoid
+    }
+    colors[i][0]=redLow;
+    colors[i][1]=redHigh;
+    colors[i][2]=greenLow;
+    colors[i][3]=greenHigh;
+    colors[i][4]=blueLow;
+    colors[i][5]=blueHigh;
+    //delay(10000);
+    redLow=-1;
+    redHigh=-1;
+    greenLow=-1;
+    greenHigh=-1;
+    blueLow=-1;
+    blueHigh=-1;
+    i++;
+  }
+  //delay(100);
+  moveConveyer();
+  //delay(100);
+  moveConveyer();
+  digitalWrite(sol3,LOW);   //turn on solenoid
+  delay(250);     //keep solonoid on for 250ms
+  digitalWrite(sol3, HIGH); //turn off solenoid
+  moveConveyer();
+  //delay(100);
+  moveConveyer();
+  //delay(100);
+  digitalWrite(sol4,LOW);   //turn on solenoid
+  delay(250);     //keep solonoid on for 250ms
+  digitalWrite(sol4, HIGH); //turn off solenoid
+  i=0;
   int j=0;
   for (i=0; i<4; i++){
     for (j=0; j<6;j++){
@@ -381,7 +303,7 @@ void calibration(){
     }
     Serial.print("\n");
   }
-  
+  Serial.print("Calibration Done\n");
 }
 
 
@@ -422,4 +344,3 @@ void getFrequencies(){
   Serial.println(blueFrequency);
   delay(100);
 }
-
